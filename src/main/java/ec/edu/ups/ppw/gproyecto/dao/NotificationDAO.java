ee9c273cfbd31efa39fe3cd80ee9d8c4053a5832
brynine <andresmejiajr10@gmail.com>
2026-02-02 01:25:03 -0500

    Migracion y conexion del backend  de programadores y horarios al frontend

--- src/main/java/ec/edu/ups/ppw/gproyecto/dao/NotificationDAO.java
+++ src/main/java/ec/edu/ups/ppw/gproyecto/dao/NotificationDAO.java
@@ -13,22 +13,18 @@ public class NotificationDAO {
 @PersistenceContext(unitName = "gproyectoPersistenceUnit")
 private EntityManager em;
 
-// INSERTAR
 public void insert(Notification n) {
     em.persist(n);
 }
 
-// BUSCAR POR ID
 public Notification read(String id) {
     return em.find(Notification.class, id);
 }
 
-// ACTUALIZAR
 public void update(Notification n) {
     em.merge(n);
 }
 
-// ELIMINAR
 public void delete(String id) {
     Notification n = read(id);
     if (n != null) {
@@ -36,7 +32,6 @@ public void delete(String id) {
     }
 }
 
-// LISTAR TODOS
 public List<Notification> getAll() {
     return em.createQuery(
             "SELECT n FROM Notification n",
